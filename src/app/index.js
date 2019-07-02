80b23a2db2ec14820ab0f0ca3d02ba1f5ab6024e
EricHeredia <EricDavidHeredia@gmail.com>
2019-07-02 02:48:27 -0500

    Started dashboard

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
-console.log("Hello World");
-
 import { store } from './store';
 
-console.log(store.getState());
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './components/Main'
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById("app")
+);
